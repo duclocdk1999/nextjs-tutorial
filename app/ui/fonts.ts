d16c042878f049bb1d3d45f49c99176cb95af49e
Loc Nguyen <duclocdk1999@gmail.com>
2025-04-13 15:12:31 +0700

    init

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Lusitana, Courier_Prime } from 'next/font/google';
+
+export const inter = Inter({subsets: ['latin']})
+export const lusitana = Lusitana({subsets: ['latin'], weight: ['400', '700']})
+export const courier = Courier_Prime({subsets: ['latin'], weight: ['400', '700']})
